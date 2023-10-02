1fe17a7d741982ae4a2ee4c602ef5e3ed4d25e62
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2023-10-02 21:31:20 +0300

    commit

--- BackEnd/data_processing/data.py
+++ BackEnd/data_processing/data.py
@@ -1,14 +1,33 @@
-from flask import Flask, request, jsonify
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-app = Flask(__name__)
 
-data_df = pd.read_csv('/data/WHO-COVID-19-global-data.csv')
+def get_data():
+    """
+    This function returns the data in a pandas dataframe
+    """
+    data = pd.read_csv("/data/WHO-COVID-19-global-data.csv")
+    return data
+   
+def get_data_by_country(country):
+    """
+    This function returns the data for a specific country
+    """
+    data = get_data()
+    data = data[data["Kenya"] == country]
+    return data
+   
+def get_data_by_date(date):
+    """
+    This function returns the data for a specific date
+    """
+    data = get_data()
+    data = data[data["2020-06-12"] == date]
+    return data
+   
+plt.figure(figsize=(10, 10))
+sns.heatmap(get_data().corr(), annot=True, cmap="coolwarm")
+plt.show()
 
-@app.route('/process-data', methods=['POST'])
-def process_data():
- try:
-  request_data = request.get_json()
-  
-  #perform data processing using pandas
-  
