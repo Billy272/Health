1690830091a032a4410fa32290ff1a67cc110f14
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2023-10-03 10:35:13 +0300

    commit

--- frontend/src/components/DataProcessing/DataProcessing.js
+++ frontend/src/components/DataProcessing/DataProcessing.js
@@ -11,7 +11,7 @@ function DataProcessing() {
 
  const fetchData = async () => {
   try {
-   const response = await axios.get('/api/get-data');
+   const response = await axios.get('../data/WHO-COVID-19-global-data.csv');
    
   if(response.data) {
    setData(response.data);
@@ -33,18 +33,27 @@ function DataProcessing() {
      <table>
       <thead>
        <tr>
-        <th>Id</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Age</th>
+        <th>Date_reported</th>
+        <th>Country_code</th>
+        <th>Country</th>
+        <th>WHO_region</th>
+        <th>New_cases</th>
+        <th>Cumulative_cases</th>
+        <th>New_deaths</th>
+        <th>Cumulative_deaths</th>
        </tr>
       </thead>
       <tbody>
        {data.map((item) => (
         <tr key={item.id}>
+         <td>{item.date}</td>
          <td>{item.id}</td>
          <td>{item.name}</td>
-         <td>{item.value}</td>
+         <td>{item.region}</td>
+         <td>{item.new_cases}</td>
+         <td>{item.cumulative_cases}</td>
+         <td>{item.new_deaths}</td>
+         <td>{item.cumulative_deaths}</td>
         </tr>
        ))}
       </tbody>
