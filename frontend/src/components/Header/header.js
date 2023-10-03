e50119e1d35b477bac3e804f44051d2a71332caa
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2023-10-03 11:28:59 +0300

    commit

---
+++ frontend/src/components/Header/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+ return (
+  <head>
+   <div className='header'>
+    <h1>COVID-19 Student Data Analsis.</h1>
+    <nav>
+     <ul>
+      <li><a>Analyzed Data</a></li>
+      <li><a>Problems Encountered</a></li>
+      <li><a>Conclusions</a></li>
+      <li><a>Solutions</a></li>
+      <li><a>Contact</a></li>
+     </ul>
+    </nav>
+   </div>
+  </head>
+ );
+}
